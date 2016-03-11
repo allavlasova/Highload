d608d6051935b220149999b734ea1cb18cc3ec9c
allavlasova <allavlasova93@mail.ru>
2016-03-11 19:17:06 +0300

    cpu

--- src/hw_app.erl
+++ src/hw_app.erl
@@ -5,12 +5,20 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 -define(PORT, 8080).
+-define(CPUS, 1).
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
     %case hw_sup:start_link(?PORT) of
+    %io:format("Args: ~p\n", [_StartArgs]),
+    case ?CPUS > 0 of
+        true ->
+            erlang:system_flag(schedulers_online, ?CPUS);
+        false ->
+            ok
+    end,
     case hw_sup:start_link(?PORT) of
 		{ok, Pid} ->
 			{ok, Pid};
